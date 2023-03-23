99159f3d855e1c03d8ed30356515da8961d127ba
Veronika Bandaevskaya <v.bandaevskaya@mail.ru>
2023-03-23 23:31:58 +0700

    Добавлена иконка и экран загрузки

---
+++ app/src/main/java/com/example/cinema/presentation/signup/SignUpFragment.kt
@@ -0,0 +1,8 @@
+package com.example.cinema.presentation.signup
+
+import androidx.fragment.app.Fragment
+import com.example.cinema.databinding.FragmentSignUpBinding
+
+class SignUpFragment: Fragment() {
+    private lateinit var binding: FragmentSignUpBinding
+}

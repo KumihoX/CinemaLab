106c0005ea1633be75b4d2a94bed81b32947ca26
Veronika Bandaevskaya <v.bandaevskaya@mail.ru>
2023-03-24 19:09:48 +0700

    Реализована верстка SignUpScreen

--- app/src/main/java/com/example/cinema/presentation/signup/SignUpFragment.kt
+++ app/src/main/java/com/example/cinema/presentation/signup/SignUpFragment.kt
@@ -1,8 +1,26 @@
 package com.example.cinema.presentation.signup
 
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.cinema.R
 import com.example.cinema.databinding.FragmentSignUpBinding
+import com.example.cinema.databinding.FragmentSplashBinding
 
 class SignUpFragment: Fragment() {
     private lateinit var binding: FragmentSignUpBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val mainView = inflater.inflate(R.layout.fragment_sign_up, container, false)
+        binding = FragmentSignUpBinding.bind(mainView)
+
+        return binding.root
+    }
 }

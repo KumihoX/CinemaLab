881cfb35ce121389397e948bb231ef8a3341f282
Veronika Bandaevskaya <v.bandaevskaya@mail.ru>
2023-03-27 20:25:52 +0700

    Squashed commit of the following:
    commit dbdcdda2cf12825fd3b79369fd0c0e564564639f
    Author: Veronika Bandaevskaya <v.bandaevskaya@mail.ru>
    Date:   Mon Mar 27 20:10:14 2023 +0700
    
        Готовый запрос регистрации
    
    commit 00497c889dd819a6d7250e4995237eb71375f5c7
    Author: Veronika Bandaevskaya <v.bandaevskaya@mail.ru>
    Date:   Fri Mar 24 22:11:01 2023 +0700
    
        Добавлена проверка одного поля на пустоту
    
    commit 106c0005ea1633be75b4d2a94bed81b32947ca26
    Author: Veronika Bandaevskaya <v.bandaevskaya@mail.ru>
    Date:   Fri Mar 24 19:09:48 2023 +0700
    
        Реализована верстка SignUpScreen

--- app/src/main/java/com/example/cinema/presentation/signup/SignUpFragment.kt
+++ app/src/main/java/com/example/cinema/presentation/signup/SignUpFragment.kt
@@ -1,8 +1,81 @@
 package com.example.cinema.presentation.signup
 
+import android.app.AlertDialog
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.example.cinema.R
 import com.example.cinema.databinding.FragmentSignUpBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class SignUpFragment: Fragment() {
     private lateinit var binding: FragmentSignUpBinding
+    private val viewModel: SignUpViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val mainView = inflater.inflate(R.layout.fragment_sign_up, container, false)
+        binding = FragmentSignUpBinding.bind(mainView)
+
+        addClickListenersOnButtons()
+
+        return binding.root
+    }
+
+    private fun addClickListenersOnButtons() {
+        registerButtonClick()
+        iHaveAccButtonClick()
+    }
+
+    private fun registerButtonClick() {
+        binding.registerButton.setOnClickListener{
+            validateFields()
+        }
+    }
+
+    private fun iHaveAccButtonClick() {
+        binding.iHaveAccButton.setOnClickListener{
+            navigateToSignInFragment()
+        }
+    }
+
+    private fun navigateToSignInFragment() {
+
+    }
+
+    private fun navigateToMainFragment() {
+
+    }
+
+    private fun createErrorDialog() {
+        val message = viewModel.message.value
+        val builder = AlertDialog.Builder(context)
+
+        builder.setTitle("Неправильно введенные данные")
+        builder.setMessage(message)
+        builder.show()
+    }
+
+    private fun validateFields() {
+        viewModel.validateEditTexts(
+            binding.nameEditText.text.toString(),
+            binding.surnameEditText.text.toString(),
+            binding.emailEditText.text.toString(),
+            binding.passwordEditText.text.toString(),
+            binding.repeatPasswordEditText.text.toString()
+        )
+
+        if (viewModel.allFieldsValid.value == false) {
+            createErrorDialog()
+        }
+        else {
+            navigateToMainFragment()
+        }
+    }
 }
---
+++ app/src/main/java/com/example/cinema/presentation/signup/SignUpViewModel.kt
@@ -0,0 +1,117 @@
+package com.example.cinema.presentation.signup
+
+import android.util.Patterns
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.cinema.data.remote.dto.RegistrationBodyDto
+import com.example.cinema.domain.usecase.signup.RegisterUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SignUpViewModel @Inject constructor(
+    private val registerUseCase: RegisterUseCase,
+): ViewModel() {
+
+    private val _allFieldsValid = MutableLiveData(true)
+    val allFieldsValid: LiveData<Boolean> = _allFieldsValid
+
+    private val _serverErrors = MutableLiveData(false)
+    val serverErrors: LiveData<Boolean> = _serverErrors
+
+    private val _message = MutableLiveData("")
+    val message: LiveData<String> = _message
+
+    fun validateEditTexts(
+        name: String,
+        surname: String,
+        email: String,
+        password: String,
+        repeatPassword: String
+    ) {
+        _message.value = ""
+        _allFieldsValid.value = true
+        nameValidityCheck(name)
+        surnameValidityCheck(surname)
+        emailValidityCheck(email)
+        passwordValidityCheck(password)
+        repeatPasswordValidityCheck(password, repeatPassword)
+
+        if (allFieldsValid.value == true) {
+            register(name, surname, email, password)
+        }
+    }
+
+    private fun register(
+        name: String,
+        surname: String,
+        email: String,
+        password: String
+    ) {
+        val userData = RegistrationBodyDto(
+            email = email,
+            firstName = name,
+            lastName = surname,
+            password = password
+        )
+
+        viewModelScope.launch {
+            registerUseCase(userData)
+        }
+    }
+
+    private fun nameValidityCheck(name: String) {
+        if (name.isEmpty()) {
+            _allFieldsValid.value = false
+            _message.value += "Заполните поле \"Имя\"\n"
+            return
+        }
+    }
+
+    private fun surnameValidityCheck(surname: String) {
+        if (surname.isEmpty()) {
+            _allFieldsValid.value = false
+            _message.value += "Заполните поле \"Фамилия\"\n"
+            return
+        }
+    }
+
+    //TODO: Переписать паттерн проверки email
+    private fun emailValidityCheck(email: String) {
+        if (email.isEmpty()) {
+            _allFieldsValid.value = false
+            _message.value += "Заполните поле \"E-mail\"\n"
+            return
+        }
+        if (!email.let { Patterns.EMAIL_ADDRESS.matcher(it).matches() }) {
+            _allFieldsValid.value = false
+            _message.value += "Введенный email не соответствует шаблону: example@mail.ru\n"
+            return
+        }
+    }
+
+    private fun passwordValidityCheck(password: String) {
+        if (password.isEmpty()) {
+            _allFieldsValid.value = false
+            _message.value += "Заполните поле \"Пароль\"\n"
+            return
+        }
+    }
+
+    private fun repeatPasswordValidityCheck(password: String, repeatPassword: String) {
+        if (repeatPassword.isEmpty()) {
+            _allFieldsValid.value = false
+            _message.value += "Заполните поле \"Повторите пароль\"\n"
+            return
+        }
+        if (password != repeatPassword) {
+            _allFieldsValid.value = false
+            _message.value += "Введенные пароли не совпадают\n"
+            return
+        }
+    }
+
+}
